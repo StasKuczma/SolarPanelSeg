c65a6d53b9fc32f25e635096e2f3b06cf2cc2fb6
Stanislaw <staskuczma@gmail.com>
2024-12-17 14:48:43 +0100

    init

---
+++ docker/run_gpu.sh
@@ -0,0 +1,37 @@
+xhost +local:root
+
+# BUILD THE IMAGE
+IMAGE="land_cover_seg"
+CONTAINER="LAND_COVER_SEG"
+
+XAUTH=/tmp/.docker.xauth
+ if [ ! -f $XAUTH ]
+ then
+     xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
+     if [ ! -z "$xauth_list" ]
+     then
+         echo $xauth_list | xauth -f $XAUTH nmerge -
+     else
+         touch $XAUTH
+     fi
+     chmod a+r $XAUTH
+ fi
+
+docker stop $CONTAINER || true && docker rm $CONTAINER || true
+
+docker run -it \
+    --gpus all \
+    --env="DISPLAY=$DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --env="XAUTHORITY=$XAUTH" \
+    --volume="$XAUTH:$XAUTH" \
+    --volume= "../src/:/workspace/src" \
+    --env="NVIDIA_VISIBLE_DEVICES=all" \
+    --env="NVIDIA_DRIVER_CAPABILITIES=all" \
+    --privileged \
+    --network=host \
+    --name="$CONTAINER" \
+    $IMAGE \
+    /bin/bash
+
